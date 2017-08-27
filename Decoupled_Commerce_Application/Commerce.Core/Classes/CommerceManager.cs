a54c181a11bd2c5c770b231d4341058fb8e44dad
gauravgahlot <gauravgahlot@quickdevnotes.com>
2017-08-27 16:42:59 +0530

    Updating the payment processing logic.

--- Decoupled_Commerce_Application/Commerce.Core/Classes/CommerceManager.cs
+++ Decoupled_Commerce_Application/Commerce.Core/Classes/CommerceManager.cs
@@ -1,25 +1,23 @@
 ﻿using Commerce.Shared.Models;
 using Commerce.Shared.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Commerce.Core
 {
     public class CommerceManager
     {
         private readonly IStoreRepository _storeRepository;
+        private readonly ICustomerValidator _customerValidator;
         private readonly IPaymentProcessor _paymentProcessor;
         private readonly ICustomerNotifier _customerNotifier;
         private readonly ILogger _logger;
 
         public CommerceManager(IStoreRepository storeRepository,
+            ICustomerValidator customerValidator,
             IPaymentProcessor paymentProcessor,
             ICustomerNotifier customerNotifier, ILogger logger)
         {
             _storeRepository = storeRepository;
+            _customerValidator = customerValidator;
             _paymentProcessor = paymentProcessor;
             _customerNotifier = customerNotifier;
             _logger = logger;
@@ -27,8 +25,22 @@ namespace Commerce.Core
 
         public bool ProcessOrder(Order order)
         {
-            
-            return true;
+            var result = false;
+            if (_customerValidator.ValidateCustomer(order.Customer))
+            {
+                foreach(var lineItem in order.LineItems)
+                {
+                    // updating store inventory
+                    _storeRepository.UpdateInventoryForProduct(lineItem.Id, lineItem.Quantity);
+
+                    // processing the order payment
+                    result = _paymentProcessor.ProcessPayment(order.PaymentDetails);
+
+                    // notifying the customer for order status
+                    _customerNotifier.NotifyCustomer(result);
+                }
+            }
+            return result;
         }
     }
 }
