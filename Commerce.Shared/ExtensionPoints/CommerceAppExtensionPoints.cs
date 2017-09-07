529ae2ebce303486b92f9ef60a6edbd607a37ffc
gauravgahlot <gauravgahlot@quickdevnotes.com>
2017-09-07 21:21:33 +0530

    Added extension points to commerce application.

---
+++ Commerce.Shared/ExtensionPoints/CommerceAppExtensionPoints.cs
@@ -0,0 +1,14 @@
+﻿namespace Commerce.Shared.ExtensionPoints
+{
+    public delegate void CommereAppExtension<T>(T args);
+
+    public interface ICommerceAppEvents
+    {
+        CommereAppExtension<OrderItemProcessedEventArgs> OrderItemProcessed { get; set; }
+    }
+
+    public class CommerceAppExtensionPoints : ICommerceAppEvents
+    {
+        public CommereAppExtension<OrderItemProcessedEventArgs> OrderItemProcessed { get; set; }
+    }
+}
---
+++ Commerce.Shared/ExtensionPoints/OrderItemProcessedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using Commerce.Shared.Models;
+using System.ComponentModel;
+
+namespace Commerce.Shared.ExtensionPoints
+{
+    public class OrderItemProcessedEventArgs : CancelEventArgs
+    {
+        public Product LineItem { get; set; }
+    }
+}
